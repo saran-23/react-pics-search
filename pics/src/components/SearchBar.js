8659dce18e602f8b69bcad5f4f7a951bf06002d2
saran-23 <romansaran446@gmail.com>
2021-01-05 17:50:30 +0530

    api integration completed (axios) and imageList Compo created

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 state = {term: ''};
-onFormSubmit(event){
+onFormSubmit = event =>{
     event.preventDefault();
-}
-    render() {
+    console.log(this.state.term);
+
+    this.props.onSubmit(this.state.term);
+};
+        render() {
         return ( 
             <div className="ui segment">
          <form onSubmit={this.onFormSubmit} className="ui form">
              <label>Image Search </label>
              <div className="field">
         <input type="text"
-         value = {this.state.term} onChange={e=> this.setState({term: e.target.value})} /> 
+         value = {this.state.term}
+          onChange={e=> this.setState({term: e.target.value})} /> 
         </div>
         </form>   
         </div>
